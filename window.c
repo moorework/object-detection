134e251bc2f75157d5d0805b6f21b9f447025bed
James Moore <moore.work@live.com>
2012-04-20 00:34:48 -0400

    Added window code

---
+++ window.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <cv.h>
+#include <highgui.h>
+
+#define DEBUG 1
+
+// compiled with
+//gcc example.c -o example `pkg-config --cflags --libs opencv`
+
+
+int window(IplImage * Query, IplImage Test)
+{
+	int query_hieght, query_width, test_height, test_width;
+        int i,j,k;
+/
+
+
+}
