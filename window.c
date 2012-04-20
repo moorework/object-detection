7d696e4c998614ee51324111e81792f72aa4682b
James Moore <moore.work@live.com>
2012-04-20 00:58:54 -0400

    new file:   slidingwindow.c 	modified:   window.c 	modified:   Makefile

--- window.c
+++ window.c
@@ -8,12 +8,38 @@
 // compiled with
 //gcc example.c -o example `pkg-config --cflags --libs opencv`
 
+//Do we need to check the size of the image?
 
-int window(IplImage * Query, IplImage Test)
+CvMat** window(IplImage * Query, IplImage Test)
 {
-	int query_hieght, query_width, test_height, test_width;
-        int i,j,k;
-/
+	int query_height = Query->height;
+	int query_width = Query->width;
+	int test_height = Test->height;
+	int test_width = Test->width;
+	char * dat
+
+	int i,j,k,l;
+
+	//FIXME This math may need work 
+	int numhoriz = test_width - (query_height/2);
+	int numvert = test_height - (query_height/2);
+
+	CvMat ** patches = malloc(numhoriz * numvert * sizeof(char) * sizeof(CvMat));
+
+	for(i = 0; i < numhoriz; i++)
+	{
+		for(j = 0; j < numvert; j++)
+		{
+			for(k = 0; k < query_width; k++)
+			{
+				for(l = 0; l < query_height; l++)
+				{
+
+				}
+			}
+		}
+	}	
 
 
+	return patches;	
 }
